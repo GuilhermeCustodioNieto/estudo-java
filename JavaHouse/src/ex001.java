21b85b7cfdb13e7b15a2805524422204bfd21821
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2023-04-04 21:46:35 -0300

    hello world
    um começo em java, indo em direção a programação back-end

---
+++ JavaHouse/src/ex001.java
@@ -0,0 +1,10 @@
+public class Ex001 {
+    public static void main(String[] args) {
+        //atribuição de variaveis para a soma
+        int n1 = 1;
+        int n2 = 2;
+        //soma dos numeros e apresentação
+        int s = n1 + n2;
+        System.out.println("Concatenação em Java é chato, mas a soma é " + s);
+    }
+}
